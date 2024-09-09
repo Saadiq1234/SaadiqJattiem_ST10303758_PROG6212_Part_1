26e2bc581af47c6681a37d3aa6121beda96673cc
Moghammad Saadiq Jattiem <ST10303758@rcconnect.edu.za>
2024-09-09 18:46:53 +0200

    made the submit claim page

---
+++ SaadiqJattiem_ST10303758_PROG6212_Part_1/Sign in for academic manager.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SaadiqJattiem_ST10303758_PROG6212_Part_1
+{
+    /// <summary>
+    /// Interaction logic for Sign_in_for_academic_manager.xaml
+    /// </summary>
+    public partial class Sign_in_for_academic_manager : Window
+    {
+        public Sign_in_for_academic_manager()
+        {
+            InitializeComponent();
+        }
+    }
+}
